98569a84a8837ad1a341fa943fbdc88d251efec4
Hidélgio Novela <hidelgionovela@gmail.com>
2023-02-22 22:20:49 +0200

    Update BookController.java

--- src/main/java/com/bookstore/bookstore_spring/controller/BookController.java
+++ src/main/java/com/bookstore/bookstore_spring/controller/BookController.java
@@ -3,27 +3,26 @@ package com.bookstore.bookstore_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class BookController {
-         
-         @GetMapping("/")
-         public String home() {
-             return "home";
-         }
 
-         @GetMapping("/book_register")
-         public String bookRegister() {
-            return "bookRegister";
-        }
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 
-        @GetMapping("/book_list")
-         public String bookList() {
-            return "bookList";
-        }
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
 
-        @GetMapping("/my_book")
-         public String myBook() {
-            return "myBook";
-        }
+    @GetMapping("/book_list")
+    public String bookList() {
+        return "bookList";
+    }
+
+    @GetMapping("/my_book")
+    public String myBook() {
+        return "myBook";
+    }
 }
