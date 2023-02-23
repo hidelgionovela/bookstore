4d339dc6c2ade917d591bdd3a68fb3e67ce7b79d
Hidélgio Novela <hidelgionovela@gmail.com>
2023-02-23 19:00:03 +0200

    Update BookRepository.java

--- src/main/java/com/bookstore/bookstore_spring/repository/BookRepository.java
+++ src/main/java/com/bookstore/bookstore_spring/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.bookstore.bookstore_spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.bookstore.bookstore_spring.entity.Book;
 
-
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer>{
          
 }
