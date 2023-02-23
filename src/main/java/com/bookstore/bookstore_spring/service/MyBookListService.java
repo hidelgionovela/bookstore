22bd5ebf6948a91dc15be076bb60f2a73e1cde68
Hidélgio Novela <hidelgionovela@gmail.com>
2023-02-23 20:25:12 +0200

    Create MyBookListService.java

---
+++ src/main/java/com/bookstore/bookstore_spring/service/MyBookListService.java
@@ -0,0 +1,25 @@
+package com.bookstore.bookstore_spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.bookstore_spring.entity.MyBookList;
+import com.bookstore.bookstore_spring.repository.MyBookRepository;
+
+@Service
+public class MyBookListService {
+
+         @Autowired
+         private MyBookRepository mybook;
+
+         public void saveMyBook(MyBookList book) {
+                  mybook.save(book);
+         }
+
+         // public List<Book> getAllBook(){
+         //          return bRepo.findAll();
+         // }
+         
+}
