3d818de2a0ea7d3774763fed3b18f78ac40d1e9f
Hidélgio Novela <hidelgionovela@gmail.com>
2023-02-27 00:35:54 +0200

    Update MyBookController.java

--- src/main/java/com/bookstore/bookstore_spring/controller/MyBookController.java
+++ src/main/java/com/bookstore/bookstore_spring/controller/MyBookController.java
@@ -1,5 +1,23 @@
 package com.bookstore.bookstore_spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bookstore.bookstore_spring.service.MyBookListService;
+
+@Controller
 public class MyBookController {
+
+         @Autowired
+         private MyBookListService service;
+
+         @RequestMapping("/deleteMyList/{id}")
+         public String deleteMyList(@PathVariable("id") int id) {
+                  service.deleteById(id);
+                  return "redirect:/my_book";
+                  // return "redirect:/book_list";
+         }
          
 }
