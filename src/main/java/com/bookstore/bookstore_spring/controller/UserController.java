7d1115a9692c06a926b87c6ab29f09530c087ee7
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-07 20:07:22 +0200

    Create UserController.java

---
+++ src/main/java/com/bookstore/bookstore_spring/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.bookstore.bookstore_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import com.bookstore.bookstore_spring.service.*;
+import com.bookstore.bookstore_spring.dto.*;
+
+
+@Controller
+@RequestMapping("/userRegister")
+public class UserController {
+
+         private UserService userService;
+
+         public UserController(UserService userService) {
+                  this.userService = userService;
+         }
+
+         @ModelAttribute("user")
+         public UserRegisterDTO returnNrUserRegisterDTO() {
+                  return new UserRegisterDTO();
+
+         }
+
+         @GetMapping
+         public String showFormUser(){
+                  return "register";
+         }
+
+         @PostMapping
+         public String registerUser(@ModelAttribute("user") UserRegisterDTO registerDTO) {
+              userService.save(registerDTO);   
+              return "redirect:/registro?Sucess";
+         }
+
+         
+
+
+}
