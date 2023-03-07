8f3497d108556ce2440208b7350e3b2d36fbf382
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-07 20:07:05 +0200

    Update UserRepository.java

--- src/main/java/com/bookstore/bookstore_spring/repository/UserRepository.java
+++ src/main/java/com/bookstore/bookstore_spring/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.bookstore.bookstore_spring.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.bookstore.bookstore_spring.entity.User;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Integer>{
          
 }
