a6aa1af90ca6221a5282b3b315a3e39c64f38607
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-07 20:07:10 +0200

    Create UserServiceImplt.java

---
+++ src/main/java/com/bookstore/bookstore_spring/service/UserServiceImplt.java
@@ -0,0 +1,42 @@
+package com.bookstore.bookstore_spring.service;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.bookstore.bookstore_spring.dto.UserRegisterDTO;
+import com.bookstore.bookstore_spring.entity.Rol;
+import com.bookstore.bookstore_spring.entity.User;
+import com.bookstore.bookstore_spring.repository.UserRepository;
+
+/**
+ * UsuarioServiceImplemets
+ */
+@Service
+public class UserServiceImplt implements UserService {
+
+         private UserRepository userRepository;
+
+         
+
+         public UserServiceImplt(UserRepository userRepository) {
+                  this.userRepository = userRepository;
+         }
+
+
+
+         @Override
+         public User save(UserRegisterDTO registroDTO) {
+                  // TODO Auto-generated method stub
+                  
+                  User usuario = new User(registroDTO.getFirst_name(),
+                  registroDTO.getLast_name(),
+                  registroDTO.getEmail(),
+                  registroDTO.getPassword(),
+                  Arrays.asList(new Rol("ROLE_USER") ));
+
+                  return userRepository.save(usuario);
+         }
+
+         
+}
