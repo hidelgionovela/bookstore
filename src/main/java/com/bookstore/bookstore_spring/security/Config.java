469246f52880059957b20d9fba6bbe06c0231e8d
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-10 23:00:40 +0200

    Create Config.java

---
+++ src/main/java/com/bookstore/bookstore_spring/security/Config.java
@@ -0,0 +1,9 @@
+package com.bookstore.bookstore_spring.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+
+public class Config extends WebSecurityConfiguration{
+
+}
