dd17f29d8310e404a162f1bb77c45840d58117b3
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-11 21:11:59 +0200

    Update UserController.java

--- src/main/java/com/bookstore/bookstore_spring/controller/UserController.java
+++ src/main/java/com/bookstore/bookstore_spring/controller/UserController.java
@@ -5,35 +5,31 @@ import org.springframework.web.bind.annotation.*;
 import com.bookstore.bookstore_spring.service.*;
 import com.bookstore.bookstore_spring.dto.*;
 
-
 @Controller
 @RequestMapping("/userRegister")
 public class UserController {
 
-         private UserService userService;
+     private UserService userService;
 
-         public UserController(UserService userService) {
-                  this.userService = userService;
-         }
+     public UserController(UserService userService) {
+          this.userService = userService;
+     }
 
-         @ModelAttribute("user")
-         public UserRegisterDTO returnNrUserRegisterDTO() {
-                  return new UserRegisterDTO();
+     @ModelAttribute("user")
+     public UserRegisterDTO returnNrUserRegisterDTO() {
+          return new UserRegisterDTO();
 
-         }
+     }
 
-         @GetMapping
-         public String showFormUser(){
-                  return "register";
-         }
+     @GetMapping
+     public String showFormUser() {
+          return "register";
+     }
 
-         @PostMapping
-         public String registerUser(@ModelAttribute("user") UserRegisterDTO registerDTO) {
-              userService.save(registerDTO);   
-              return "redirect:/userRegister?sucess";
-         }
-
-         
-
+     @PostMapping
+     public String registerUser(@ModelAttribute("user") UserRegisterDTO registerDTO) {
+          userService.save(registerDTO);
+          return "redirect:/userRegister?sucess";
+     }
 
 }
