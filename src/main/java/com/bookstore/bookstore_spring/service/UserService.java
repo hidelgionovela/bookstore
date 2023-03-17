caee2a1a3a4c11d65d1ae0aa1b60232371e4844b
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-17 23:36:03 +0200

    Update UserService.java

--- src/main/java/com/bookstore/bookstore_spring/service/UserService.java
+++ src/main/java/com/bookstore/bookstore_spring/service/UserService.java
@@ -1,8 +1,10 @@
 package com.bookstore.bookstore_spring.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.bookstore.bookstore_spring.dto.UserRegisterDTO;
 import com.bookstore.bookstore_spring.entity.User;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
          public User save(UserRegisterDTO registroDTO);
 }
