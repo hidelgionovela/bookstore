b23e37d1be6813b89ab94e3b90a1e44ea82c98a8
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-19 22:32:00 +0200

    Update Config.java

--- src/main/java/com/bookstore/bookstore_spring/security/Config.java
+++ src/main/java/com/bookstore/bookstore_spring/security/Config.java
@@ -1,49 +1,31 @@
 package com.bookstore.bookstore_spring.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.bookstore.bookstore_spring.service.UserService;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
 
-     @Autowired
-     private UserService userService;
-
-     @Bean
-     public BCryptPasswordEncoder passwordEncoder() {
-         return new BCryptPasswordEncoder();
-     }
-
-     @Bean
-     public DaoAuthenticationProvider authenticationProvider(){
-         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-         auth.setUserDetailsService((UserDetailsService) userService);
-         auth.setPasswordEncoder(passwordEncoder());
-         return auth;
-     }
-
-//      @Override
-     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-         auth.authenticationProvider(authenticationProvider());
-     }
-
-     @Override
-     protected void configure (HttpSecurity htttp) throws Exception{
-         htttp.authorizeHttpRequests().
-     }
-
-     
-
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authConfig -> {
+				authConfig.requestMatchers("/").permitAll();
+				authConfig.requestMatchers("/login2").permitAll();
+				authConfig.requestMatchers("/user/**").authenticated();
+				authConfig.requestMatchers("/admin/**").denyAll();
+				
+			})
+			.formLogin(Customizer.withDefaults()) // Login with browser and Form
+			.httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
+		return http.build();
+	}
+	
 }
-// 
