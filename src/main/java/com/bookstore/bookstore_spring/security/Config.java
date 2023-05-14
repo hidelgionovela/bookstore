1a3d196d6415185e276e67c1e66de50d2f4e5a60
Hidélgio Novela <hidelgionovela@gmail.com>
2023-05-14 21:40:30 +0200

    Update Config.java

--- src/main/java/com/bookstore/bookstore_spring/security/Config.java
+++ src/main/java/com/bookstore/bookstore_spring/security/Config.java
@@ -1,29 +1,29 @@
-package com.bookstore.bookstore_spring.security;
+// package com.bookstore.bookstore_spring.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.Customizer;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class Config {
+// @Configuration
+// @EnableWebSecurity
+// public class Config {
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeHttpRequests(authConfig -> {
-					authConfig.requestMatchers("/").permitAll();
-					authConfig.requestMatchers("/login2").permitAll();
-					authConfig.requestMatchers("/user/**").authenticated();
-					authConfig.requestMatchers("/admin/**").denyAll();
+// 	@Bean
+// 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+// 		http
+// 				.authorizeHttpRequests(authConfig -> {
+// 					authConfig.requestMatchers("/").permitAll();
+// 					authConfig.requestMatchers("/login2").permitAll();
+// 					authConfig.requestMatchers("/user/**").authenticated();
+// 					authConfig.requestMatchers("/admin/**").denyAll();
 
-				})
-				.formLogin(Customizer.withDefaults()) // Login with browser and Form
-				.httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
-		return http.build();
-	}
+// 				})
+// 				.formLogin(Customizer.withDefaults()) // Login with browser and Form
+// 				.httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
+// 		return http.build();
+// 	}
 
-}
+// }
